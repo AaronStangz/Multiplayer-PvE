109d59fdb4655e91f145fe9957258980019f6123
AaronStangz <Aaronstangline.as@gmail.com>
2024-08-15 12:56:17 -0400

    0.2
    Lobby Hosting

---
+++ Multiplayer-PvE/Assets/Scripts/PlayerObjectController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+
+public class PlayerObjectController : NetworkBehaviour
+{
+    //Player data
+    [SyncVar] public int ConnectionID;
+    [SyncVar] public int PlayerIdNumber;
+    [SyncVar] public ulong PlayerSteamID;
+    [SyncVar(hook = nameof(PlayerNameUpdate))] public string PlayerName;
+    [SyncVar(hook = nameof(PlayerReadyUpdate))] public bool Ready;
+
+    private CustomNetweorkManager manager;
+
+    private CustomNetweorkManager Manager
+    {
+        get
+        {
+            if (manager != null)
+            {
+                return manager;
+            }
+            return manager = CustomNetweorkManager.singleton as CustomNetweorkManager;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void PlayerReadyUpdate(bool oldValue, bool newValue)
+    {
+        if (isServer) //Host
+        {
+            this.Ready = newValue;
+        }
+        if (isClient) //Client
+        {
+            LobbyController.instance.UpdatePlayerList();
+        }
+    }
+
+    [Command]
+    private void CmdSetPlayerReady()
+    {
+        this.PlayerReadyUpdate(this.Ready, !this.Ready);
+    }
+
+    public void ChangeReady()
+    {
+        if (isOwned)
+        {
+            CmdSetPlayerReady();
+        }
+
+    }
+
+    public override void OnStartAuthority()
+    {
+        CmdSetPlayerName(SteamFriends.GetPersonaName().ToString());
+        gameObject.name = "LocalGamePlayer";
+        LobbyController.instance.FindLocalPlayer();
+        LobbyController.instance.UpdateLobbyName();
+    }
+    public override void OnStartClient()
+    {
+        Manager.GamePlayers.Add(this);
+        LobbyController.instance.UpdateLobbyName();
+        LobbyController.instance.UpdatePlayerList();
+    }
+
+    public override void OnStopClient()
+    {
+        Manager.GamePlayers.Remove(this);
+        LobbyController.instance.UpdateLobbyName();
+
+
+    }
+
+    [Command]
+    private void CmdSetPlayerName(string PlayerName)
+    {
+        this.PlayerNameUpdate(this.PlayerName, PlayerName);
+    }
+
+    public void PlayerNameUpdate(string OldValue, string NewValue)
+    {
+        if (isServer) //Host
+        {
+            this.PlayerName = NewValue;
+        }
+        if (isClient) //Client
+        {
+            LobbyController.instance.UpdatePlayerList();
+        }
+    }
+
+    //Start Game
+
+    public void CanStartGame(string SceneName)
+    {
+        if (isOwned)
+        {
+            CmdCanStartGame(SceneName);
+        }
+    }
+
+    [Command]
+    public void CmdCanStartGame(string SceneName)
+    {
+        manager.StartGame(SceneName);
+    }
+}
